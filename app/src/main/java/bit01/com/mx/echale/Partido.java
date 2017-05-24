0446518f5597c7ab8495066e29bc7790a80e9a74
Erick Lara <ericklarita@gmail.com>
2017-05-24 01:48:52 -0500

    Recycler view de partidos y perfil

---
+++ app/src/main/java/bit01/com/mx/echale/Partido.java
@@ -0,0 +1,75 @@
+package bit01.com.mx.echale;
+
+/**
+ * Created by ericklara on 24/05/17.
+ */
+
+public class Partido {
+    private String localTeam;
+    private String awayTeam;
+    private String date;
+    private String localTeamImageUrl;
+    private String awayTeamImageUrl;
+    private String time;
+
+
+    public Partido() {
+    }
+
+    public Partido(String localTeam, String awayTeam, String date, String localTeamImageUrl, String awayTeamImageUrl, String time) {
+        this.localTeam = localTeam;
+        this.awayTeam = awayTeam;
+        this.date = date;
+        this.localTeamImageUrl = localTeamImageUrl;
+        this.awayTeamImageUrl = awayTeamImageUrl;
+        this.time = time;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getLocalTeam() {
+        return localTeam;
+    }
+
+    public void setLocalTeam(String localTeam) {
+        this.localTeam = localTeam;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+    public void setAwayTeam(String awayTeam) {
+        this.awayTeam = awayTeam;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLocalTeamImageUrl() {
+        return localTeamImageUrl;
+    }
+
+    public void setLocalTeamImageUrl(String localTeamImageUrl) {
+        this.localTeamImageUrl = localTeamImageUrl;
+    }
+
+    public String getAwayTeamImageUrl() {
+        return awayTeamImageUrl;
+    }
+
+    public void setAwayTeamImageUrl(String awayTeamImageUrl) {
+        this.awayTeamImageUrl = awayTeamImageUrl;
+    }
+}
---
+++ app/src/main/java/bit01/com/mx/echale/PartidoAdapter.java
@@ -0,0 +1,97 @@
+package bit01.com.mx.echale;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by ericklara on 24/05/17.
+ */
+
+public class PartidoAdapter extends RecyclerView.Adapter<PartidoAdapter.PartidoViewHolder>{
+
+    private List<Partido> matchList;
+    private Context context;
+
+    public PartidoAdapter(List<Partido> matchList) {
+        this.matchList = matchList;
+    }
+
+    @Override
+    public PartidoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_partido, parent, false);
+
+        PartidoViewHolder viewHolder = new PartidoViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(PartidoViewHolder holder, int position) {
+        Partido partido = matchList.get(position);
+        holder.bindPartido(partido);
+    }
+
+    @Override
+    public int getItemCount() {
+        return matchList.size();
+    }
+
+    class PartidoViewHolder extends RecyclerView.ViewHolder{
+
+        @BindView(R.id.local_team_image)
+        CircleImageView localTeamImage;
+
+        @BindView(R.id.away_team_image)
+        CircleImageView awayTeamImage;
+
+        @BindView(R.id.local_team_name)
+        TextView localTeamName;
+
+        @BindView(R.id.away_team_name)
+        TextView awayTeamName;
+
+        @BindView(R.id.match_date)
+        TextView matchDate;
+
+        @BindView(R.id.match_time)
+        TextView matchTime;
+
+        public PartidoViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        public void bindPartido(Partido partido){
+            localTeamName.setText(partido.getLocalTeam());
+            awayTeamName.setText(partido.getAwayTeam());
+            matchDate.setText(partido.getDate());
+            matchTime.setText(partido.getTime());
+
+            if(!partido.getLocalTeamImageUrl().isEmpty()) {
+                Glide.with(context)
+                        .load(partido.getLocalTeamImageUrl())
+                        .into(localTeamImage);
+            }
+
+            if(!partido.getAwayTeamImageUrl().isEmpty()) {
+                Glide.with(context)
+                        .load(partido.getAwayTeamImageUrl())
+                        .into(awayTeamImage);
+            }
+        }
+    }
+}
