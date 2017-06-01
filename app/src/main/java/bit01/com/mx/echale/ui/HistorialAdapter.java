f7b10486ad3fc9a3274a705f3365fb6651fc4a48
Erick Lara <ericklarita@gmail.com>
2017-05-31 21:18:26 -0500

    Historil RV

---
+++ app/src/main/java/bit01/com/mx/echale/ui/HistorialAdapter.java
@@ -0,0 +1,111 @@
+package bit01.com.mx.echale.ui;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import bit01.com.mx.echale.R;
+import bit01.com.mx.echale.models.Historial;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by ericklara on 31/05/17.
+ */
+
+public class HistorialAdapter extends RecyclerView.Adapter<HistorialAdapter.HistorialViewHolder>{
+
+    private List<Historial> apuestas;
+
+    private Context context;
+
+    public HistorialAdapter(List<Historial> apuestas) {
+        this.apuestas = apuestas;
+    }
+
+    @Override
+    public HistorialViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        View view = LayoutInflater.from(context)
+                .inflate(R.layout.item_historial, parent, false);
+
+        HistorialViewHolder viewHolder = new HistorialViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(HistorialViewHolder holder, int position) {
+        Historial promocion = apuestas.get(position);
+        holder.bindHistorial(promocion);
+    }
+
+    @Override
+    public int getItemCount() {
+        return apuestas.size();
+    }
+
+    public class HistorialViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.historial_local_team_image)
+        CircleImageView localTeamImage;
+
+        @BindView(R.id.historial_away_team_image)
+        CircleImageView awayTeamImage;
+
+        @BindView(R.id.historial_local_team_name)
+        TextView localTeamName;
+
+        @BindView(R.id.historial_away_team_name)
+        TextView awayTeamName;
+
+        @BindView(R.id.historial_match_date)
+        TextView matchDate;
+
+        @BindView(R.id.historial_evento)
+        TextView evento;
+
+        @BindView(R.id.historial_monto)
+        TextView monto;
+
+        View rootView;
+
+        public HistorialViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        public void bindHistorial(Historial historial){
+            localTeamName.setText(historial.getNombreLocal());
+            awayTeamName.setText(historial.getNombreVisita());
+            matchDate.setText(historial.getFecha());
+            evento.setText("Evento: " + historial.getEvento());
+            monto.setText("Monto: " + historial.getMonto());
+
+            if (!historial.getUriLocal().isEmpty()) {
+
+                Glide.with(context)
+                        .load(historial.getUriLocal())
+                        .into(localTeamImage);
+
+
+            }
+
+            if (!historial.getUriVisita().isEmpty()) {
+
+                Glide.with(context)
+                        .load(historial.getUriVisita())
+                        .into(awayTeamImage);
+            }
+        }
+    }
+
+}
--- app/src/main/java/bit01/com/mx/echale/ui/HistorialRVActivity.java
+++ app/src/main/java/bit01/com/mx/echale/ui/HistorialRVActivity.java
@@ -2,14 +2,90 @@ package bit01.com.mx.echale.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import bit01.com.mx.echale.R;
+import bit01.com.mx.echale.models.Historial;
+import bit01.com.mx.echale.models.Partido;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HistorialRVActivity extends AppCompatActivity {
+
+    List<Historial> mListApuestas = new ArrayList<>();
+
+    @BindView(R.id.rvHistorial)
+    RecyclerView recyclerView;
+
+    FirebaseAuth mAuth;
+
+    String userUid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historial_rv);
+
+        // Inicializamos ButterKnife
+        ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        userUid = mAuth.getCurrentUser().getUid();
+
+        traerHistorial();
+
+    }
+
+    public void traerHistorial(){
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("/users/" + userUid + "/historial/");
+
+        final List<Historial> partidosApostados = new ArrayList<>();
+
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                partidosApostados.clear();
+
+                Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+                if(!children.equals(null)) {
+                    for (DataSnapshot child : children) {
+                        partidosApostados.add(child.getValue(Historial.class));
+                    }
+
+                    mListApuestas = partidosApostados;
+                    settingRecyclerView();
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void settingRecyclerView() {
+
+        HistorialAdapter historialAdapter = new HistorialAdapter(mListApuestas);
+        recyclerView.setLayoutManager(new LinearLayoutManager(HistorialRVActivity.this));
+        recyclerView.setAdapter(historialAdapter);
     }
 }
